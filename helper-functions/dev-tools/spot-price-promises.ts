0674e4789f93f5ac05e2ae05621291379ba4ef67
Amir0reza <white_mage@Amir>
2022-12-06 22:19:39 +0100

    1. spot price updated to do less blockchain call 2. Getter price with Promise added in scrips

--- helper-functions/dev-tools/spot-price-promises.ts
+++ helper-functions/dev-tools/spot-price-promises.ts
@@ -1,21 +1,47 @@
+import exchangesV2CompatibleJSON from "../json-files/exchanges-uniswap-v2-compatible.json"
+import uniswapV2PairsJSON from "../json-files/uniswap-v2-pairs.json"
+
 import { uniswapV2SpotPrice } from "../uniswap-helper"
+import { tokenNameAddressConvertor } from "./token-name-address-conversion"
 
 /**
  * @dev The promise to get spot price of exchanges at once.
- * @notice The exchanges are predefined base on liquidity to avoid usage of low liquid markets
+ * @param baseToken The base token address to get the price for
+ * @param quoteToken The quote token to get the price for
  * @return List of objects with the name of exchange and the prices
  */
-export const WMATIC_ETH = new Promise<Object[]>(async (resolve) => {
-    const baseToken = "weth",
-        quoteToken = "wmatic",
-        factoryAddress1 = "0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32",
-        factoryAddress2 = "0xc35DADB65012eC5796536bD9864eD8773aBc74C4"
-    const price1: number = Number(
-            await uniswapV2SpotPrice(baseToken, quoteToken, factoryAddress1)
-        ),
-        price2: number = Number(
-            await uniswapV2SpotPrice(baseToken, quoteToken, factoryAddress2)
-        )
+export const spotPricePromise = function (
+    baseToken: string,
+    quoteToken: string
+) {
+    return new Promise<Object>(async (resolve) => {
+        let pricesObj: any = {}
+        let baseTokenSymbol = baseToken,
+            quoteTokenSymbol = quoteToken
+        if (baseToken.length === 42) {
+            baseTokenSymbol = tokenNameAddressConvertor(baseToken)
+            quoteTokenSymbol = tokenNameAddressConvertor(quoteToken)
+        }
 
-    resolve([price1, price2])
-})
+        const pairName: string = `${baseTokenSymbol.toUpperCase()}-${quoteTokenSymbol.toUpperCase()}`,
+            mirrorPairName: string = `${quoteTokenSymbol.toUpperCase()}-${baseTokenSymbol.toUpperCase()}`
+
+        for (let i = 0; i < exchangesV2CompatibleJSON.length; i++) {
+            const factoryAddress = exchangesV2CompatibleJSON[i].Factory
+
+            if (
+                uniswapV2PairsJSON[i].pairs[pairName] !== undefined ||
+                uniswapV2PairsJSON[i].pairs[mirrorPairName] !== undefined
+            ) {
+                pricesObj[exchangesV2CompatibleJSON[i].name] =
+                    await uniswapV2SpotPrice(
+                        baseToken,
+                        quoteToken,
+                        factoryAddress
+                    )
+            }
+        }
+
+        resolve(pricesObj)
+    })
+}
